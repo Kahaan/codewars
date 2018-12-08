ddcfd872d6d8ca18a46ac01c898011e04c89f6b1
Kahaan <kahaan.r.patel@gmail.com>
2018-12-08 11:08:03 -0800

    dail algorightim

--- codewars.js
+++ codewars.js
@@ -1,4 +1,6 @@
 
+// Write a function that returns whther a given integer is a prime number
+
 function isPrime(num) {
   if(num === 1 || num === 0 || num < 0){
     return false
@@ -9,4 +11,36 @@ function isPrime(num) {
     }
   }
   return true
+}
+
+// Write a function, persistence, that takes in a positive parameter num and
+// returns its multiplicative persistence, which is the number of times you must
+// multiply the digits in num until you reach a single digit.
+
+
+
+function abc(num) {
+
+  let digits = num.toString()
+
+    digits = digits.split("").map((num) => (parseInt(num)))
+    digits = digits.reduce(function(acc,el){
+        return acc * el
+    })
+
+    return digits
+
+}
+
+function persistence(num){
+  let count = 0
+  while(true){
+
+    if(num > 9){
+      count++
+      num = abc(num)
+    } else {
+      return count
+    }
+  }
 }
