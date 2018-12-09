112cd1a6cd22051e7afeb7e70c48286c16bc1aae
Kahaan <kahaan.r.patel@gmail.com>
2018-12-09 13:20:27 -0800

    daily algorithim

--- codewars.js
+++ codewars.js
@@ -44,3 +44,32 @@ function persistence(num){
     }
   }
 }
+
+// You are going to be given an array of integers. Your job is to take that
+// array and find an index N where the sum of the integers to the left of N is 
+// equal to the sum of the integers to the right of N. If there is no index that
+//  would make this happen, return -1.
+
+
+function findEvenIndex(arr) {
+
+
+  if (arr[0] === arr.slice(1).reduce((acc,el) => (acc + el))){
+    return 0
+  } else if(arr[arr.length - 1] === arr.slice(0,-1).reduce((acc,el) => (acc + el))) {
+    return arr.length - 1
+  }
+
+
+  for(let i = 1; i < arr.length -1; i++){
+
+    let startSum = arr.slice(0,i).reduce((acc,el) => (acc + el))
+    let endSum = arr.slice(i + 1,arr.length).reduce((acc,el) => (acc + el))
+    if (startSum === endSum){
+      return i
+    }
+
+  }
+  return -1
+
+}
