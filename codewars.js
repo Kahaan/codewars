f73af33750ef5e1e03750adf179ce223ba955c94
Kahaan <kahaan.r.patel@gmail.com>
2018-12-16 11:48:13 -0800

    daily algorithm

--- codewars.js
+++ codewars.js
@@ -72,13 +72,13 @@ function findEvenIndex(arr) {
 // so that it returns the sum of all the multiples of 3 or 5 below the number
 // passed in. Note: If the number is a multiple of both 3 and 5, only count it once.
 
-function solution(number){
-  if (number <= 0) return 0
-  let arr = []
-  for(let i = 0; i < number; i++){
-    if(i % 3 === 0 || i % 5 === 0){
-       arr.push(i)
-    }
-  }
-  return arr.reduce((acc,el) => (acc + el))
-}
+// function solution(number){
+//   if (number <= 0) return 0
+//   let arr = []
+//   for(let i = 0; i < number; i++){
+//     if(i % 3 === 0 || i % 5 === 0){
+//        arr.push(i)
+//     }
+//   }
+//   return arr.reduce((acc,el) => (acc + el))
+// }
