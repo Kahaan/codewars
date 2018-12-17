a5f688bf071853fa9ab54d5ca491048e2443a9ad
Kahaan <kahaan.r.patel@gmail.com>
2018-12-17 15:26:27 -0800

    daily algorithm

--- codewars.js
+++ codewars.js
@@ -1,46 +1,40 @@
-
 // Write a function that returns whther a given integer is a prime number
 
 function isPrime(num) {
-  if(num === 1 || num === 0 || num < 0){
-    return false
+  if (num === 1 || num === 0 || num < 0) {
+    return false;
   }
-  for(let i = 2; i < num; i++){
-    if(num % i === 0){
-      return false
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
     }
   }
-  return true
+  return true;
 }
 
 // Write a function, persistence, that takes in a positive parameter num and
 // returns its multiplicative persistence, which is the number of times you must
 // multiply the digits in num until you reach a single digit.
 
-
-
 function abc(num) {
+  let digits = num.toString();
 
-  let digits = num.toString()
+  digits = digits.split("").map(num => parseInt(num));
+  digits = digits.reduce(function(acc, el) {
+    return acc * el;
+  });
 
-    digits = digits.split("").map((num) => (parseInt(num)))
-    digits = digits.reduce(function(acc,el){
-        return acc * el
-    })
-
-    return digits
-
+  return digits;
 }
 
-function persistence(num){
-  let count = 0
-  while(true){
-
-    if(num > 9){
-      count++
-      num = abc(num)
+function persistence(num) {
+  let count = 0;
+  while (true) {
+    if (num > 9) {
+      count++;
+      num = abc(num);
     } else {
-      return count
+      return count;
     }
   }
 }
@@ -50,21 +44,15 @@ function persistence(num){
 // equal to the sum of the integers to the right of N. If there is no index that
 //  would make this happen, return -1.
 
-
 function findEvenIndex(arr) {
-
-
-  for(let i = 1; i < arr.length -1; i++){
-
-    let startSum = arr.slice(0,i).reduce((acc,el) => (acc + el))
-    let endSum = arr.slice(i + 1,arr.length).reduce((acc,el) => (acc + el))
-    if (startSum === endSum){
-      return i
+  for (let i = 1; i < arr.length - 1; i++) {
+    let startSum = arr.slice(0, i).reduce((acc, el) => acc + el);
+    let endSum = arr.slice(i + 1, arr.length).reduce((acc, el) => acc + el);
+    if (startSum === endSum) {
+      return i;
     }
-
   }
-  return -1
-
+  return -1;
 }
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
@@ -72,13 +60,13 @@ function findEvenIndex(arr) {
 // so that it returns the sum of all the multiples of 3 or 5 below the number
 // passed in. Note: If the number is a multiple of both 3 and 5, only count it once.
 
-function solution(number){
-  if (number <= 0) return 0
-  let arr = []
-  for(let i = 0; i < number; i++){
-    if(i % 3 === 0 || i % 5 === 0){
-       arr.push(i)
-    }
-  }
-  return arr.reduce((acc,el) => (acc + el))
-}
+// function solution(number){
+//   if (number <= 0) return 0
+//   let arr = []
+//   for(let i = 0; i < number; i++){
+//     if(i % 3 === 0 || i % 5 === 0){
+//        arr.push(i)
+//     }
+//   }
+//   return arr.reduce((acc,el) => (acc + el))
+// }
