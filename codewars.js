962f5f87cce2cb8edd3876c6014c38f10d2bdd33
Kahaan <kahaan.r.patel@gmail.com>
2018-12-20 12:52:07 -0800

    daily algorithm

--- codewars.js
+++ codewars.js
@@ -113,10 +113,10 @@ function cakes(recipe, available) {
 // that should return the total number of smiling faces.
 //
 // Rules for a smiling face:
--Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
--A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
--Every smiling face must have a smiling mouth that should be marked with either ) or D.
-No additional characters are allowed except for those mentioned.
+// -Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// -A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// -Every smiling face must have a smiling mouth that should be marked with either ) or D.
+// No additional characters are allowed except for those mentioned.
 //
 // countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
 // countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
